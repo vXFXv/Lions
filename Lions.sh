d30ea583ef115c90516b491291fa1d1b32773e15
Lions team <89591744+AbsLionsTEAM@users.noreply.github.com>
2021-08-27 12:44:43 +0300

    Update and rename DevProx.sh to Lions.sh

--- Lions.sh
+++ Lions.sh
@@ -3,7 +3,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 Version=`lsb_release -rs | cut -f1 -d"."`
 install() {
 echo -e "\033[0;36m"
-echo -e "~ Welcome To Source DevProx - اهلا بك في سورس ديف بروكس ~"
+echo -e "  ~ Welcome To Source Lions - اهلا بك في سورس الاسود ~"
 echo -e "     ~ { Setting Up Offices - جاري رفع المكاتب } ~"
 echo -e "\033[0m"
 sudo apt-get update -y
@@ -42,21 +42,8 @@ sudo luarocks install Lua-cURL
 sudo luarocks install luautf8
 cd .. ;sudo rm -rf luarocks*
 sudo timedatectl set-timezone Asia/Baghdad
-echo -e "\033[0;36m"
-echo -e "---------------------------------------------"
-echo -e "|    ____             ____                  |"
-echo -e "|   |  _ \  _____   _|  _ \ _ __ _____  __  |"
-echo -e "|   | | | |/ _ \ \ / / |_) | '__/ _ \ \/ /  |"
-echo -e "|   | |_| |  __/\ V /|  __/| | | (_) >  <   |"
-echo -e "|   |____/ \___| \_/ |_|   |_|  \___/_/\_\  |"
-echo -e "|-------------------------------------------|"
-echo -e "|This Source Was Developed By (ABS) @IQ_ABS.|"
-echo -e "|  This Is The Source Channel @Dev_Prox .   |"
-echo -e "|               - DevProx -                 |"
-echo -e "---------------------------------------------"
-echo -e "\033[0m"
 cd $THIS_DIR && sudo chmod +x tg
-lua DevProx.lua
+lua Lions.lua
 }
 if [ "$1" = "install" ]; then
 install
@@ -66,5 +53,5 @@ echo -e "\033[0;31m\n~ The tg File Was Not Found In The Bot Files\n\033[0;33m"
 exit 1
 fi
 cd $THIS_DIR && sudo chmod +x tg
-lua DevProx.lua
+lua Lions.lua
 fi
